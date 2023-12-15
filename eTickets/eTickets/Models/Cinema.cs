a7b9e7bb85573247a9ca37ca89def81acb4bd8e1
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-12-15 19:20:46 +0000

    Created view for cinemas

--- eTickets/eTickets/Models/Cinema.cs
+++ eTickets/eTickets/Models/Cinema.cs
@@ -13,10 +13,13 @@ namespace eTickets.Models
 
         public int Id { get; set; }
 
+        [Display(Name = "Cinema Logo")]
         public string Logo { get; set; }
 
+        [Display(Name = "Name")]
         public string Name { get; set; }
 
+        [Display(Name = "Description")]
         public string Description { get; set; }
 
         //Relationships

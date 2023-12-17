f29c1eacef67aed5e3b700bb8b307749f4e4486d
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-12-17 23:42:21 +0000

    Update and Delete async methods added to base repository

--- eTickets/eTickets/Data/Base/EntityBaseRepository.cs
+++ eTickets/eTickets/Data/Base/EntityBaseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,9 +20,11 @@ namespace eTickets.Data.Base
         public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
         
 
-        public Task DeleteAsync(int id)
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var entity = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Deleted;
         }
 
         public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
@@ -32,9 +35,10 @@ namespace eTickets.Data.Base
             
         
 
-        public Task<T> UpdateAsync(int id, T entity)
+        public async Task UpdateAsync(int id, T entity)
         {
-            throw new NotImplementedException();
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Modified;
         }
     }
 }

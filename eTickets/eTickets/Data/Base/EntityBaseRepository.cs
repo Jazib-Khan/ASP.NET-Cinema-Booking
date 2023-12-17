737d16c031b63edb421ae8df268f3f61372c3b08
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-12-17 23:30:04 +0000

    Add async() method in base repository

--- eTickets/eTickets/Data/Base/EntityBaseRepository.cs
+++ eTickets/eTickets/Data/Base/EntityBaseRepository.cs
@@ -1,11 +1,40 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace eTickets.Data.Base
 {
-    public class EntityBaseRepository
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
     {
+
+        private readonly AppDbContext _context;
+
+        public EntityBaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
+        
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+            
+        
+
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+            
+        
+
+        public Task<T> UpdateAsync(int id, T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

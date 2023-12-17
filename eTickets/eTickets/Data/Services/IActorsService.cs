c06f596466b2a7de470cae908f52dedac88ca9fc
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-12-17 19:22:54 +0000

    Added actors service

--- eTickets/eTickets/Data/Services/IActorsService.cs
+++ eTickets/eTickets/Data/Services/IActorsService.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using eTickets.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace eTickets.Data.Services
 {
-    interface IActorsService
+    public interface IActorsService
     {
+        Task<IEnumerable<Actor>> GetAll();
+        
+        Actor GetById(int id);
+
+        void Add(Actor actor);
+
+        Actor Update(int id, Actor newActor);
+
+        void Delete(int id);
     }
 }

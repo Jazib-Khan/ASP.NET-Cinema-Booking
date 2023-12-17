13703f3a43996aa26d4e16b2abfa3402de4db1a1
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-12-17 20:30:45 +0000

    Adding actor data to db

--- eTickets/eTickets/Controllers/ActorsController.cs
+++ eTickets/eTickets/Controllers/ActorsController.cs
@@ -1,5 +1,6 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -28,6 +29,17 @@ namespace eTickets.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("FullName, ProfilePictureUrl, Bio")]Actor actor)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            _service.Add(actor);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- eTickets/eTickets/Models/Actor.cs
+++ eTickets/eTickets/Models/Actor.cs
@@ -14,12 +14,19 @@ namespace eTickets.Models
         public int Id { get; set; }
 
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is Required")]
+
         public string ProfilePictureUrl { get; set; }
 
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is Required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
+
         public string FullName { get; set; }
 
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is Required")]
+
         public string Bio { get; set; }
 
         // Relationships

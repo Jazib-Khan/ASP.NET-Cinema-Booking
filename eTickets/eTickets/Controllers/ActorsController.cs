d270bfc41ec3884ee901f1e5799e5263f14823d3
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-12-17 22:02:16 +0000

    Actors details can be updated on edit actors

--- eTickets/eTickets/Controllers/ActorsController.cs
+++ eTickets/eTickets/Controllers/ActorsController.cs
@@ -47,8 +47,29 @@ namespace eTickets.Controllers
         {
             var actorDetails = await _service.GetByIdAsync(id);
 
-            if (actorDetails == null) return View("Empty");
+            if (actorDetails == null) return View("Not Found");
             return View(actorDetails);
+        }
+
+        //Get: Actors/Edit
+        public async Task <IActionResult> Edit(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            if (actorDetails == null) return View("Not Found");
+
+            return View(actorDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, FullName, ProfilePictureUrl, Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            await _service.UpdateAsync(id, actor);
+            return RedirectToAction(nameof(Index));
         }
     }
 }

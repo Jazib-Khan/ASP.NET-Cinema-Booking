c2c585ab9ad714d24e743933fa558809bd99d6c3
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-12-21 16:04:30 +0000

    Adding new producers to database

--- eTickets/eTickets/Models/Producer.cs
+++ eTickets/eTickets/Models/Producer.cs
@@ -15,12 +15,18 @@ namespace eTickets.Models
         public int Id { get; set; }
 
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is required")]
         public string ProfilePictureUrl { get; set; }
 
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 characters")]
+
         public string FullName { get; set; }
 
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
+
         public string Bio { get; set; }
 
         // Relationships

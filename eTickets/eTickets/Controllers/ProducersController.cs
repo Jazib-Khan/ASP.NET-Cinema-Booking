ba018f6984e750fa03dc1bf921ecbf47868a9e98
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-12-25 21:43:44 +0000

    Producers can be deleted

--- eTickets/eTickets/Controllers/ProducersController.cs
+++ eTickets/eTickets/Controllers/ProducersController.cs
@@ -74,5 +74,25 @@ namespace eTickets.Controllers
             
         }
 
+        //GET: producers/delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+
+            return View(producerDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+
+        }
+
     }
 }

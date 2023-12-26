3d10da485fcdda1ca7165a3015d206a91848e1b2
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-12-26 18:56:40 +0000

    Getting cinema details

--- eTickets/eTickets/Controllers/CinemasController.cs
+++ eTickets/eTickets/Controllers/CinemasController.cs
@@ -40,5 +40,14 @@ namespace eTickets.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        //Get: Cinemas/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
     }
 }

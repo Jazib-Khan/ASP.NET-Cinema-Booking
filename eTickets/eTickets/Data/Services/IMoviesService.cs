91093999557ca821d04b6b6e4b3c4f27ebd9dfb6
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-12-28 22:39:02 +0000

    User can update movies

--- eTickets/eTickets/Data/Services/IMoviesService.cs
+++ eTickets/eTickets/Data/Services/IMoviesService.cs
@@ -13,6 +13,8 @@ namespace eTickets.Data.Services
         Task<Movie> GetMovieByIdAsync(int id);
         Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues();
         Task AddNewMovieAsync(NewMovieVM data);
+        Task UpdateMovieAsync(NewMovieVM data);
+
 
     }
 }

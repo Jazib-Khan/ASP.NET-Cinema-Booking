64f7b359a7fc586b4625ced5c71cb4a93cc51c9a
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-01-01 22:58:46 +0000

    Added identity tables

--- eTickets/eTickets/Controllers/OrdersController.cs
+++ eTickets/eTickets/Controllers/OrdersController.cs
@@ -22,6 +22,14 @@ namespace eTickets.Controllers
             _shoppingCart = shoppingCart;
             _ordersService = ordersService;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            string userId = "";
+
+            var orders = await _ordersService.GetOrdersByUserIdAsync(userId);
+            return View(orders);
+        }
         
         public IActionResult ShoppingCart()
         {

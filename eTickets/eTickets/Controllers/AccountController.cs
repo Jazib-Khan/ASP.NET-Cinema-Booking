d6ac43c410cca6c60982130803819f6bf8b5f922
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-01-02 23:32:50 +0000

    Designed login view

--- eTickets/eTickets/Controllers/AccountController.cs
+++ eTickets/eTickets/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using eTickets.Data;
+using eTickets.Data.ViewModels;
 using eTickets.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -24,9 +25,7 @@ namespace eTickets.Controllers
 
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Login() => View(new LoginVM());
+
     }
 }

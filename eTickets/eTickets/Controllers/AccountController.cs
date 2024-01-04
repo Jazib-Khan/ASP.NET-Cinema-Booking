4eb8de92cbf1c940fb0748f040ac82d407fad363
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-01-04 22:34:01 +0000

    Signing out users

--- eTickets/eTickets/Controllers/AccountController.cs
+++ eTickets/eTickets/Controllers/AccountController.cs
@@ -80,9 +80,14 @@ namespace eTickets.Controllers
                 await _userManager.AddToRoleAsync(newUser, UserRoles.User);
 
             return View("RegisterCompleted");
+                        
+        }
 
-            
-
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Movies");
         }
 
     }
